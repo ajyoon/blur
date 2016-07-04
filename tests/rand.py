b1e5e0113d108ab2e8fa5d5da3c7e8a21a07e4c3
Andrew Yoon <andrewyoon2@gmail.com>
2016-07-04 02:45:56 -0400

    Add test scaffolding for rand.py

--- tests/rand.py
+++ tests/rand.py
@@ -1,9 +1,49 @@
 import unittest
 import rand
 
+
 class TestRand(unittest.TestCase):
+    def test__linear_interp(self):
+        # TODO: Build me
+        pass
+
+    def test__point_under_curve(self):
+        # TODO: Build me
+        pass
+
+    def test_prob_bool(self):
+        # TODO: Build me
+        pass
+
+    def test_percent_possible(self):
+        # TODO: Build me
+        pass
+
+    def test_pos_or_neg(self):
+        # TODO: Build me
+        pass
+
+    def test_pos_or_neg_1(self):
+        # TODO: Build me
+        pass
+
+    def test_merge_markov_weights_dicts(self):
+        # TODO: Build me
+        pass
 
     def test_weighted_option_rand(self):
         for i in range(1000):
             options = [(0, 1), (5, 2), (10, 5)]
             self.assertTrue(rand.weighted_option_rand(options) in [0, 5, 10])
+
+    def test_interp_dict(self):
+        # TODO: Build me
+        pass
+
+    def test_weighted_curve_rand(self):
+        # TODO: Build me
+        pass
+
+    def test_weighted_sort(self):
+        # TODO: Build me
+        pass

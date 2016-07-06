369776cb3dcee3a43604047e490f527f6863a120
Andrew Yoon <andrewyoon2@gmail.com>
2016-07-05 20:33:03 -0400

    More tests for Graph

--- tests/graph.py
+++ tests/graph.py
@@ -35,6 +35,11 @@ class TestGraph(unittest.TestCase):
                                    nodes.Node('Node 5')])
         self.assertEqual(len(self.test_graph.node_list), 5)
 
+    def test_add_nodes_with_merge_existing_names(self):
+        self.test_graph.add_nodes(nodes.Node('Node 3'),
+                                  merge_existing_names=True)
+        self.assertEqual(len(self.test_graph.node_list), 3)
+
     def test_feather_links(self):
         # Todo: Build me
         pass
@@ -47,6 +52,10 @@ class TestGraph(unittest.TestCase):
         found_node = self.test_graph.find_node_by_name('Node 2')
         self.assertEqual(found_node, self.node_2)
 
+    def test_find_node_by_name_with_invalid_name(self):
+        with self.assertRaises(ValueError):
+            self.test_graph.find_node_by_name('bogus name')
+
     def test_remove_node(self):
         self.test_graph.remove_node(self.node_2)
         self.assertEqual(self.test_graph.node_list,
@@ -55,21 +64,58 @@ class TestGraph(unittest.TestCase):
         self.assertEqual(len(self.test_graph.node_list[1].link_list), 1)
 
     def test_remove_node_by_name(self):
-        # Todo: Build me
-        pass
+        self.test_graph.remove_node_by_name('Node 1')
+        self.assertEqual(self.test_graph.node_list,
+                         [self.node_2, self.node_3])
+        # Test that no links point to any node named 'Node 1'
+        for n in self.test_graph.node_list:
+            self.assertFalse('Node 1' in [l.target.name for l in n.link_list])
+
+    def test_remove_node_by_name_with_invalid_name(self):
+        self.test_graph.remove_node_by_name('bogus name')
+        self.assertEqual(self.test_graph.node_list,
+                         [self.node_1, self.node_2, self.node_3])
 
     def test_has_node_with_name(self):
         self.assertTrue(self.test_graph.has_node_with_name('Node 1'))
         self.assertFalse(self.test_graph.has_node_with_name('bogus name'))
 
     def test_pick_by_use_weight(self):
-        # Todo: Build me
-        pass
+        self.node_1.use_weight = 10000
+        self.node_2.use_weight = 1
+        self.node_3.use_weight = 1
+        # Repeatedly test and verify probability within a margin of error
+        # This is not fool-proof.
+        # Would be neat in to do significant statistical verification
+        # of rand.py where more solid tests would be appropriate.
+        trial_count = 100
+        node_1_count = 0
+        for i in range(trial_count):
+            if self.test_graph.pick_by_use_weight() == self.node_1:
+                node_1_count += 1
+        self.assertTrue(node_1_count > (trial_count * 0.6))
 
-    def test_pick(self):
-        # Todo: Build me
-        pass
+    def test_pick_with_no_starting_or_current_node(self):
+        # Pick with starting_node=None and
+        # self.test_graph.current_node=None, should pick from use_weight
+        picked_node = self.test_graph.pick(starting_node=None)
+        self.assertTrue(picked_node in self.test_graph.node_list)
+        # Test that self.test_graph.current_node correctly updated
+        self.assertEqual(self.test_graph.current_node, picked_node)
 
-    def test_walk(self):
-        # Todo: Build me
-        pass
+    def test_pick_with_no_starting_node_but_with_current_node(self):
+        # Pick with starting_node=None, but with a
+        # valid self.test_graph.current_node
+        self.test_graph.current_node = self.node_1
+        picked_node = self.test_graph.pick(starting_node=None)
+        self.assertTrue(picked_node in [self.node_2, self.node_3])
+        # Test that self.test_graph.current_node correctly updated
+        self.assertEqual(self.test_graph.current_node, picked_node)
+
+    def test_pick_with_starting_node(self):
+        # Pick with a starting_node
+        picked_node = self.test_graph.pick(starting_node=self.node_1)
+        self.assertTrue(picked_node in [self.node_2, self.node_3])
+        # Test that self.test_graph.current_node correctly updated
+        self.assertEqual(self.test_graph.current_node, picked_node)
+

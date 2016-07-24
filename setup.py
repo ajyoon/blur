e18ccaf2f58a3371a9f6d11a8791a4501efb2ea3
Andrew Yoon <andrewyoon2@gmail.com>
2016-07-24 19:18:48 -0400

    Remove incorrect software category

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',

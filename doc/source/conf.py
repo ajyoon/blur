80985657c2a2210358fcc023fd8489a0baf777eb
Andrew Yoon <andrewyoon2@gmail.com>
2016-07-24 16:47:16 -0400

    Prepare docs for RTD integration

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,10 +37,6 @@ sys.path.insert(0, os.path.abspath('../../..'))
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
     'sphinx.ext.napoleon',
     'sphinxcontrib.fulltoc'
 ]

99375e28452bbf92fb3cc00c57dc91d1288c24f6
Andrew Yoon <andrewyoon2@gmail.com>
2016-07-24 17:43:41 -0400

    Fix typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='blur',
     version='0.1',
-    packages=['blur', 'blur.markov']
+    packages=['blur', 'blur.markov'],
     description='A chance art toolkit.',
     author='Andrew Yoon',
     author_email='andrewyoon2@gmail.com',

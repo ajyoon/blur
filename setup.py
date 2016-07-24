8dc8dcabbd21f3926e4fcb13cdac2447c9402245
Andrew Yoon <andrewyoon2@gmail.com>
2016-07-24 17:01:59 -0400

    Prepare PYPI Release

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='blur',
+    packages=['blur'],
+    version='0.1',
+    description='A chance art toolkit.',
+    author='Andrew Yoon',
+    author_email='andrewyoon2@gmail.com',
+    url='https://github.com/ajyoon/blur',
+    # download_url='*******************',  # TODO
+    keywords=['random', 'stochastic', 'art']
+)

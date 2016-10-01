e19c874e174d4bcc1874d14020c7fd7cbf9016a2
Andrew Yoon <andrewyoon2@gmail.com>
2016-10-01 03:51:33 -0400

    Polish softlife example

--- examples/softlife.py
+++ examples/softlife.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 """
 Conway's Game Of Life using fuzzy values everywhere.
+
+This is an example of some of the things ``blur.soft`` can do.
 """
 
 # Python 2/3 agnostic tk import
@@ -10,16 +12,6 @@ except ImportError:
     import Tkinter as tk
 
 from blur import soft
-
-tk_host = tk.Tk()
-
-CANVAS_WIDTH = 640
-CANVAS_HEIGHT = 640
-
-canvas = tk.Canvas(tk_host,
-                   width=CANVAS_WIDTH,
-                   height=CANVAS_HEIGHT)
-canvas.pack()
 
 
 class SoftCell:
@@ -40,16 +32,6 @@ class SoftCell:
         self.color_dead = soft.SoftColor(0, 0, 0)
         self.value = value
         self.allow_change = allow_change
-
-# Fill the world
-CELLS_PER_ROW = 80
-allow_change_weight = soft.SoftBool(0.4)
-starting_value = soft.SoftBool(0)
-world = [[None] * CELLS_PER_ROW for _ in range(CELLS_PER_ROW)]
-for x in range(CELLS_PER_ROW):
-    for y in range(CELLS_PER_ROW):
-        world[x][y] = SoftCell(starting_value.get(),
-                               allow_change_weight)
 
 
 def update_state(world):
@@ -94,25 +76,15 @@ def update_state(world):
                 if live_neighbor_count == 3:
                     world[x][y].value = True
 
-# Create canvas grid
-cell_size = 640 / CELLS_PER_ROW
-canvas_grid = [[None] * CELLS_PER_ROW for _ in range(CELLS_PER_ROW)]
-for x in range(len(world)):
-    for y in range(len(world[x])):
-        canvas_grid[x][y] = canvas.create_rectangle(
-                                x * cell_size,
-                                y * cell_size,
-                                (x * cell_size) + cell_size,
-                                (y * cell_size) + cell_size,
-                                fill="#808080")
 
 def click_event(event):
-    """On click, make the cell under the cursor alive."""
+    """On click, bring the cell under the cursor to Life"""
     grid_x_coord = int(divmod(event.x, cell_size)[0])
     grid_y_coord = int(divmod(event.y, cell_size)[0])
     world[grid_x_coord][grid_y_coord].value = True
     color = world[x][y].color_alive.get_as_hex()
     canvas.itemconfig(canvas_grid[grid_x_coord][grid_y_coord], fill=color)
+
 
 def draw_canvas():
     """Render the tkinter canvas based on the state of ``world``"""
@@ -124,15 +96,47 @@ def draw_canvas():
                 color = world[x][y].color_dead.get_as_hex()
             canvas.itemconfig(canvas_grid[x][y], fill=color)
 
+
 def refresh_canvas_and_state():
     """Callback for the main loop."""
     update_state(world)
     draw_canvas()
-    # Change ``100`` to alter the frame rate.
+    # Refresh every 100 ms
     tk_host.after(100, refresh_canvas_and_state)
 
-tk_host.bind('<Button 1>', click_event)
-tk_host.bind('<B1-Motion>', click_event)
-refresh_canvas_and_state()
-print('Click anywhere on the window to create Life!')
-tk_host.mainloop()
+
+if __name__ == '__main__':
+    # Initialize tk and canvas
+    tk_host = tk.Tk()
+    CANVAS_WIDTH = 640
+    CANVAS_HEIGHT = 640
+    canvas = tk.Canvas(tk_host,
+                       width=CANVAS_WIDTH,
+                       height=CANVAS_HEIGHT)
+    canvas.pack()
+    # Fill the world
+    CELLS_PER_ROW = 80
+    allow_change_weight = soft.SoftBool(0.4)
+    starting_value = soft.SoftBool(0)
+    world = [[None] * CELLS_PER_ROW for _ in range(CELLS_PER_ROW)]
+    for x in range(CELLS_PER_ROW):
+        for y in range(CELLS_PER_ROW):
+            world[x][y] = SoftCell(starting_value.get(),
+                                   allow_change_weight)
+    # Create canvas grid
+    cell_size = 640 / CELLS_PER_ROW
+    canvas_grid = [[None] * CELLS_PER_ROW for _ in range(CELLS_PER_ROW)]
+    for x in range(len(world)):
+        for y in range(len(world[x])):
+            canvas_grid[x][y] = canvas.create_rectangle(
+                                    x * cell_size,
+                                    y * cell_size,
+                                    (x * cell_size) + cell_size,
+                                    (y * cell_size) + cell_size,
+                                    fill="#808080")
+    # Lift off!
+    tk_host.bind('<Button 1>', click_event)
+    tk_host.bind('<B1-Motion>', click_event)
+    refresh_canvas_and_state()
+    print('Click anywhere on the window to create Life!')
+    tk_host.mainloop()
